517e4434578926eafa0c9052461bcc5b1d89bf9b
JasonJunMa <majun900313@gmail.com>
2017-10-20 05:26:28 -0400

    Chapter6

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
-#include<stdio.h>
-int main(){
-	for(int i=0;i<10;i++){
-			printf("Hello SubC");
-		}
+{
+	int five = 111;
+	{
+		haha = five;
+		int five  = -1 + 2 - 3 + 4 -3;
+		int ratio = five/9.0;
+
+		int fahrenheit = 25>72;
+
+		centigrade = 25/0;
+		fahrenheit = centigrade/ratio + 32/2;
+
+		centigrade = 25;
+		fahrenheit = 32 + centigrade/ratio;
+	}
 }
