64ead019fd7bf4ee6d978a4a80c6f46debc7176f
JasonJunMa <majun900313@gmail.com>
2017-12-18 11:31:36 -0500

    update

--- fib.c
+++ fib.c
@@ -1,5 +1,6 @@
 float fib1(int n,float c)
 {
+    writeln("value passed in ",c);
     int i,t1,t2,next;
     i = 1;
     t1= 0;
@@ -14,6 +15,7 @@ float fib1(int n,float c)
         t2 = next;
         i = i+1;
     }while(i>n);
+    c = c*5;
     return c;
 }
 
@@ -35,6 +37,7 @@ void fib2(int n)
     }
 }
 int main(){
+    writeln();
     writeln("This program will print Fibonacci sequence!Have fun!");
     writeln("1:Get Fibonacci Series up to n number of terms");
     writeln("2:Get  Fibonacci Sequence Up to a Certain Number");
@@ -48,6 +51,8 @@ int main(){
         writeln("Please enter the number of terms:");
         readln(k);
         m=fib1(k,m);
+        writeln();
+        writeln("value return from fib1 ",m);
     }
     else{
         writeln("Enter a positive number:");
